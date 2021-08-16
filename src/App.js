fe6c91dba517f26df77a95bee8dfc879070a9e93
Mohamed Mohiey <mohhiey@gmail.com>
2021-08-17 01:07:03 +0200

    implemented directory, menu item, homepage components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
-    <div >
-      Hello World
+    <div>
+      <HomePage />
     </div>
   );
 }
