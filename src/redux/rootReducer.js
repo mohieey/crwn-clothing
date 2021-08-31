d9970902e8a1eec266ccd509221505fbef20a52c
Mohamed Mohiey <mohhiey@gmail.com>
2021-08-31 04:29:16 +0200

    moved sections to store

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -4,9 +4,14 @@ import storage from "redux-persist/lib/storage";
 
 import userReducer from "./user/userReducer";
 import cartReducer from "./cart/cartReducer";
+import directoryReducer from "./directory/directoryReducer";
 
 const persistConfig = { key: "root", storage, whiteList: ["cart"] };
 
-const rootReducer = combineReducers({ user: userReducer, cart: cartReducer });
+const rootReducer = combineReducers({
+  user: userReducer,
+  cart: cartReducer,
+  directory: directoryReducer,
+});
 
 export default persistReducer(persistConfig, rootReducer);
